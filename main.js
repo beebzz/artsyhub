c068b9133bbf087f2f57ccf3751b07c1b162dfc1
Bilal Boussayoud <bbouss@Bilals-MacBook-Air.local>
2019-07-11 18:50:43 -0400

    Refining server functionality & client-side logic

--- main.js
+++ main.js
@@ -1,37 +1,28 @@
-const request = require('request');
 const express = require('express');
 const router = require('express').Router();
+const request = require('request');
 const app = express();
-//const tag = 'kehlani';
 const api_key = 'qup7qb9q3l9j9cqzrsw0d0gm'
-
-
-/*request(url, {json: true}, (err, res, body) => {
-  if(err){return console.log(err);}
-  console.log(body);
-});*/
-
-//Example
-router.get('/', function(req, res, next) {
-  res.json('Testing');
-});
 
 //Get listing id's route
 router.get('/listingids', function(req, res) {
   const artistName = req.query.artistName.replace(/s/g, '%20');
   const listingUrl = 'https://openapi.etsy.com/v2/listings/active.js?tags=' + artistName + '&api_key=' + api_key;
-  request(listingUrl, {json: true}, (err, response, body) => {
+  request(listingUrl, {jsonp: true}, (err, response, body) => {
     if(err){return console.log(err);}
-    res.json(JSON.stringify(body));
+    body = JSON.parse(body.slice(5, (body.length-2)));
+    //console.log(body[0]);
+    res.json(body.results[0].listing_id);
   })
 });
 
-//Get shop names route
+
+/*
 router.get('/shopnames', function(req, res) {
   const listingId = req.query.listingId;
   const shopUrl = 'https://openapi.etsy.com/v2/shops/listing/' + listingId + '.js?api_key=' + api_key
   res.json(listingId);
-});
+});*/
 
 app.use('/', router);
 
