0ab8e6775d9b4f9e84bcd15eff7b5d22d1e2fa6b
Bilal Boussayoud <bbouss@Bilals-Air.mlbam.mlb.org>
2019-07-12 14:55:12 -0400

    front and backend complete

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const path = require('path');
 const router = require('express').Router();
 const request = require('request');
 const app = express();
 const api_key = 'qup7qb9q3l9j9cqzrsw0d0gm'
+
+let shopUrls = [];
+
+let shopReq = function(id){
+  let promise = new Promise((resolve, reject) => {
+    const shopUrl = 'https://openapi.etsy.com/v2/shops/listing/' + id + '.js?api_key=' + api_key;
+    request(shopUrl, {jsonp: true}, (err, response, body) => {
+      if(err){reject(err);}
+      body = JSON.parse(body.slice(5, (body.length-2)));
+      resolve(body.results);
+    });
+  })
+  return promise;
+};
+
+let results = [];
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Get listing id's route
 router.get('/listingids', function(req, res) {
@@ -11,19 +30,19 @@ router.get('/listingids', function(req, res) {
   request(listingUrl, {jsonp: true}, (err, response, body) => {
     if(err){return console.log(err);}
     body = JSON.parse(body.slice(5, (body.length-2)));
-    //console.log(body[0]);
-    res.json(body.results[0].listing_id);
+    results.push(body.results[0].listing_id, body.results[1].listing_id, body.results[2].listing_id, body.results[3].listing_id, body.results[4].listing_id, body.results[5].listing_id);
+    for(let i = 0; i < results.length; i++)
+    {
+      shopUrls.push(shopReq(results[i]));
+    }
+    Promise.all(shopUrls).then(shopUrls => res.json(shopUrls.map(item => item[0].shop_name)));
   })
 });
 
-
-/*
-router.get('/shopnames', function(req, res) {
-  const listingId = req.query.listingId;
-  const shopUrl = 'https://openapi.etsy.com/v2/shops/listing/' + listingId + '.js?api_key=' + api_key
-  res.json(listingId);
-});*/
-
 app.use('/', router);
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
+});
 
 app.listen(5000, () => console.log('Listening on port 5000'));
