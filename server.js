62585eead06bc4a0343fe74fd39f27c8402e5a73
Bilal Boussayoud <bbouss@Bilals-Air.mlbam.mlb.org>
2019-07-15 16:30:07 -0400

    changed key value

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const router = require('express').Router();
 const request = require('request');
 const app = express();
 const port = process.env.PORT || 5000;
-const apikey = 'qup7qb9q3l9j9cqzrsw0d0gm';
+const key = process.env.etsykey;
 
 let shopReq = function(id){
   let promise = new Promise((resolve, reject) => {
-    const shopUrl = 'https://openapi.etsy.com/v2/shops/listing/' + id + '.js?api_key=' + apikey;
+    const shopUrl = 'https://openapi.etsy.com/v2/shops/listing/' + id + '.js?api_key=' + etsykey;
     request(shopUrl, {jsonp: true}, (err, response, body) => {
       if(err){reject(err);}
       body = JSON.parse(body.slice(5, (body.length-2)));
@@ -25,7 +25,7 @@ router.get('/listingids', function(req, res) {
   let shopUrls = [];
   let results = [];
   const artistName = req.query.artistName.replace(/s/g, '%20');
-  const listingUrl = 'https://openapi.etsy.com/v2/listings/active.js?tags=' + artistName + '&api_key=' + apikey;
+  const listingUrl = 'https://openapi.etsy.com/v2/listings/active.js?tags=' + artistName + '&api_key=' + etsykey;
   request(listingUrl, {jsonp: true}, (err, response, body) => {
     if(err){return console.log(err);}
     body = JSON.parse(body.slice(5, (body.length-2)));
